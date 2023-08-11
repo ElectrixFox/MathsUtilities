279bce47e494db6b57bb357cd27aa3fea0f45630
ElectrixFox <lukecphillips2006@gmail.com>
2023-08-11 11:41:26 +0100

    Initialised Again

---
+++ factor.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+
+std::vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+struct Factors
+    {
+    vector<int> x;
+    };
+
+int baseFermat(int n, std::vector<int>& f);
+Factors Factor(int n);
+
+
+int main(int argc, char const *argv[])
+{
+int n = 0;
+cin >> n;
+
+Factors f = Factor(n);
+
+for (int i = 0; i < f.x.size(); i++)
+    {
+    std::cout << f.x[i];
+    std::cout << (((i + 1) == f.x.size()) ? "" : "*");
+    }
+
+cin.get();
+
+return 0;
+}
+
+int baseFermat(int n, std::vector<int>& f)
+{
+
+// getting any factors of two immediately out of there
+while(n % 2 == 0)
+    {
+    n /= 2;
+    f.push_back(2);
+    }
+
+// linear search to test if prime
+auto ptest = [](int n){for(int i = 0; i < sizeof(primes) / sizeof(primes[0]); i++)
+    {
+    if(primes[i] == n)
+        return 1;
+    }
+    return 0;
+    };
+
+// fermat factorisation routine
+auto fermat = [](int n)
+    {
+    int a = 0;
+    int b = 0;
+
+    while((a*a - n) < 0) a+= 1;
+
+    while(floor(sqrt(a*a-n)) < sqrt(a*a-n))
+        {
+        a+=1;
+        }
+
+    b = sqrt(a*a-n);
+
+    return std::tuple<int, int>((a-b), (a+b));
+    };
+
+
+tuple<int, int> facs = fermat(n);
+
+// get the vals from the tuple
+int v1 = get<0>(facs);
+int v2 = get<1>(facs);
+
+
+// testing if they're prime
+int res1 = ptest(v1);
+int res2 = ptest(v2);
+
+
+// if it is a prime factor add it to the list if not factorise that (for both factors)
+if(res1 == 1)
+    f.push_back(v1);
+else
+    baseFermat(v1, f);
+
+if(res2 == 1)
+    f.push_back(v2);
+else
+    baseFermat(v2, f);
+
+
+return 1;
+}
+
+
+Factors Factor(int n)
+{
+Factors f;
+
+// factor by trial division for small numbers
+auto trialdiv = [](int n, Factors f) {
+    for(int i = 0; i < sizeof(primes) / sizeof(primes[0]); i++)
+        {
+        while(n % primes[i] == 0)
+            {
+            f.x.push_back(primes[i]);
+            n /= primes[i];
+            }
+        }
+    };
+
+
+baseFermat(n, f.x);
+
+return f;
+}
