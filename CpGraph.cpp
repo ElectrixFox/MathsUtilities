8589af9d3e88b78d404e13d68bebc1c0d22e3fbf
ElectrixFox <lukecphillips2006@gmail.com>
2023-08-14 08:48:16 +0100

    Circles and Lines
    The circles now work and draw properly. The circles can be dragged, they can be created. Lines can be drawn between them. The Python calculation has been implemented for drawing to the border. A fail safe has been added in case of division by 0.

--- CpGraph.cpp
+++ CpGraph.cpp
@@ -1,6 +1,11 @@
 #include "CpGraph.h"
 
-int n = 0;
+int n = 1;
+
+struct vec2
+{
+float x, y;
+};
 
 class Shape
 {
@@ -9,14 +14,69 @@ public:
 
     virtual void draw(QPainter* qp) {};
     
-    void coords() { printf("\n(%d, %d)", x, y); };
+    vec2 coords(int print = 0) { if(print == 1) printf("\n(%f, %f)", x, y); return {(float)x, (float)y}; };
+    virtual void move(int xto, int yto, int offset = 0) { x = xto; y = yto; bbox.moveTo(x - offset, y - offset); };
+
+    int getID() { return id; };
+
+    QRectF bbox;
+
+    int moving = 0;
 
 protected:
     int id;
     int x, y;
 };
 
-Shape::Shape(int inx, int iny) : x(inx), y(iny) {};
+Shape::Shape(int inx, int iny) : x(inx), y(iny) { id = n; n += 1;};
+
+class Text
+    {
+    public:
+
+    // constructor to create the object, defaulting "" text so that it can be empty to start for future changing
+    Text(int inx, int iny, std::string content = "");
+
+    // drawing
+    void draw(QPainter* qp);
+
+    private:
+    float x, y;
+    std::string text;
+
+    };
+
+
+Text::Text(int inx, int iny, std::string content)
+{
+// I don't like floats so I'm casting
+x = (float)inx;
+y = (float)iny;
+
+text = content;
+}
+
+void Text::draw(QPainter* qp)
+{
+// add options in the future for colour, size, font, etc...
+
+// writing the text
+qp->drawText(x, y, "Hello, Qt!");
+}
+
+class Edge
+{
+public:
+    // source and target shapes
+    Edge(Shape* insource, Shape* intarget);
+
+    // drawing
+    void draw(QPainter* qp);
+
+private:
+    Shape* source;
+    Shape* target;
+};
 
 class Circle : public Shape
 {
@@ -25,9 +85,14 @@ public:
 
     void draw(QPainter* qp) override;
 
-    int x, y, radius;
+    // overrides because of the different shapes
+    virtual void move(int xto, int yto, int offset = 0) override;
+
+    int getID() { return id; };
+    int getRadius() { return radius; };
+
 protected:
-    int id;
+    int radius;
     int moving = 0;
 };
 
@@ -37,79 +102,175 @@ x = inx;
 y = iny;
 id = n;
 n += 1;
-}
 
-void Circle::draw(QPainter* qp)
-{
-// jargon to do different things
 int a = x - radius;
 int b = y - radius;
 int c = 2*radius;
 
+bbox = QRectF(a, b, c, c);
+}
+
+void Circle::draw(QPainter* qp)
+{
 qp->setBrush(Qt::red);
 
-qp->drawEllipse(a, b, c, c);
-
+qp->drawEllipse(bbox);
 }
+
+void Circle::move(int xto, int yto, int offset)
+{
+x = xto;
+y = yto;
+
+offset = radius;
+
+bbox.moveTo(x - offset, y - offset);
+}
+
+Edge::Edge(Shape* insource, Shape* intarget)
+{
+source = insource;
+target = intarget;
+};
+
+void Edge::draw(QPainter* qp)
+{
+// assuming they're circles
+Circle* c1 = (Circle*)source;
+Circle* c2 = (Circle*)target;
+
+// giving all of these values easy to use alias'
+float x1 = c1->coords().x;
+float y1 = c1->coords().y;
+float r1 = c1->getRadius();
+
+
+float x2 = c2->coords().x;
+float y2 = c2->coords().y;
+float r2 = c2->getRadius();
+
+if((x2 - x1) == 0)
+    x1 = x2 + 0.00001f;
+
+// getting the gradient of the line between them
+float m = (y2 - y1)/(x2 - x1);
+
+// if c2 is on the other side to c1 then add 180 to make it go full 2 pi radians (360 deg)
+float O = (x2 < x1) ? M_PI : 0;
+
+
+// getting the angle between the centre of the circle and where the line passes through it's circumference
+float p = atan(m)+O;
+
+// cos of the angle between the line and the centre
+// where r1 is the hypotenuse of the line we find the horizontal distance and then offset it by x1 to make it local to the circle
+// same sort of thing with sin apart from it's used to find the vertical distance
+vec2 pos1 = {x1 + r1*cos(p), y1 + r1*sin(p)};
+
+// negative here because it is intersecting a different circle at a different point
+vec2 pos2 = {x2 - r2*cos(p), y2 - r2*sin(p)};
+
+// drawing the line
+qp->drawLine(pos1.x, pos1.y, pos2.x, pos2.y);
+};
 
 class WindowManager : public QWidget
 {
 public: 
     WindowManager(QWidget* parent = nullptr) : QWidget(parent) { setMouseTracking(true); };
 
-    std::vector<Shape*> shapes;
+    void add(Shape* shape);
+    void add(Edge* edge);
+    Shape* getShape(int index);
 
 protected:
+    std::vector<Shape*> shapes;
+    std::vector<Edge*> edges;
+    Shape* active = nullptr;
+
     void paintEvent(QPaintEvent* event) override 
     {    
     QPainter p(this);
     p.setRenderHint(QPainter::Antialiasing);
 
-    Circle c(250, 250, 40);
-    c.draw(&p);
-
-    shapes[0]->draw(&p);
-
-    /* for (Shape s : shapes)
+    for (Shape* s : shapes)
         {
-        s.coords();
-        s.draw(&p);
-        } */
+        s->draw(&p);
+        }
+    for (Edge* e : edges)
+        {
+        e->draw(&p);
+        }
     };
 
     void mousePressEvent(QMouseEvent* event) override 
-    {
-    std::cout << this->paintingActive();
-    
+    {    
     event->button();
     int px = event->pos().rx();
     int py = event->pos().ry();
 
     std::cout << "\n(" << px << ", " << py << ")";
 
-
-    /* for (int i = 0; i < shapes.size(); i++)
+    for(Shape* s : shapes)
         {
-        if(shapes[i].bbox.contains(px, py))
+        if(s->bbox.contains(px, py))
             {
-            //std::cout << "\nClicking: " << shapes[i].id;
+            std::cout << "\nClicking: " << s->getID();
+            s->moving = 1;
+            active = s;
             }
-        } */
+        }
+
     };
 
-/*         void mouseMoveEvent(QMouseEvent* event) override 
+    void mouseMoveEvent(QMouseEvent* event) override 
+    {
+    if(active != nullptr)
         {
+        if(active->moving == 1)
+            {
+            int x = event->pos().rx();
+            int y = event->pos().ry();
+            
+            active->move(x, y);
+            update();
+            }
+        }
+    };
 
-        update();
-        };
+    void mouseReleaseEvent(QMouseEvent* event) override
+    {
+    int px = event->pos().rx();
+    int py = event->pos().ry();
 
-        void mouseReleaseEvent(QMouseEvent* event) override
+    if(active != nullptr)
         {
-
-        update();
-        }; */
+        if(active->bbox.contains(px, py))
+            {
+            std::cout << "\nStopped Moving: " << active->getID();
+            
+            active->moving = 0;
+            active = nullptr;
+            }
+        }
+    };
 
 };
+
+void WindowManager::add(Shape* shape)
+{
+shapes.push_back(shape);
+}
+
+void WindowManager::add(Edge* edge)
+{
+edges.push_back(edge);
+}
+
+Shape* WindowManager::getShape(int index)
+{
+return shapes[index];
+}
 
 
 
@@ -118,20 +279,20 @@ int CpMain()
 int h = 0;
 QApplication app(h, {});
 
-/* QWidget wind;
-wind.setWindowTitle("Ellipse Drawing Example");
-wind.setGeometry(100, 100, 400, 400);
-wind.show(); */
-
 WindowManager winman;
+winman.setWindowTitle("Graphy");
 
-Circle c1(150, 150, 10);
-Circle c2(0, 0, 25);
-winman.shapes.push_back(&c1);
-winman.shapes.push_back(&c2);
+Circle* c1 = new Circle(150, 150, 25);
+Circle* c2 = new Circle(50, 50, 25);
+Edge* e = new Edge(c1, c2);
+winman.add(c1);
+winman.add(c2);
+winman.add(e);
 
 winman.setGeometry(100, 100, 400, 400);
 winman.show();
+
+QRectF f = winman.getShape(0)->bbox;
 
 int result = app.exec();
 
